6535c52e43c588c1b17005e08192cd8a1f4503b5
Tek Nath Acharya <teknatha136@gmail.com>
2022-12-11 14:33:10 +0600

    Make kubectl executable

--- kubectl-setup.sh
+++ kubectl-setup.sh
@@ -8,5 +8,5 @@ curl -LO "https://dl.k8s.io/$KUBECTL_VERSION/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
 
 mkdir $GITHUB_WORKSPACE/bin
-mv kubectl $GITHUB_WORKSPACE/bin
+mv kubectl $GITHUB_WORKSPACE/bin && chmod +x $GITHUB_WORKSPACE/bin/kubectl 
 echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
