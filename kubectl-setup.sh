df544c9fefb718930317431af2c8fced9392e0ce
Tek Nath Acharya <31593478+teknatha136@users.noreply.github.com>
2023-03-06 12:31:16 +0600

    change path to something unique

--- kubectl-setup.sh
+++ kubectl-setup.sh
@@ -7,6 +7,6 @@ curl -LO "https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$KUBECTL_VERSION/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
 
-mkdir $GITHUB_WORKSPACE/bin
-mv kubectl $GITHUB_WORKSPACE/bin && chmod +x $GITHUB_WORKSPACE/bin/kubectl 
+mkdir $GITHUB_WORKSPACE/kubebin
+mv kubectl $GITHUB_WORKSPACE/kubebin && chmod +x $GITHUB_WORKSPACE/kubebin/kubectl 
 echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
